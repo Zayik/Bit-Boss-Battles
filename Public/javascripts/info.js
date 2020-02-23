a710046423bad23935b1a8aae4d796c43d7b3e4b
Zayik <davidleeanderson1991@gmail.com>
2020-02-23 02:47:22 -0800

    Remove old client access token.
    Remove old client access token.

--- Public/javascripts/info.js
+++ Public/javascripts/info.js
@@ -1,6 +1,5 @@
 // The client ID
 var clientId = "xmabmufjjekqhsbfqenk9yml72ckc3";
-// var accessToken = "bbw6gddl5glvuapi3wvip8vd82vroz"
 // The redirect URI
 var redirectUri = "http://localhost:5500/Public/auth.html";
 var baseUri = "http://localhost:5500/Public/";
